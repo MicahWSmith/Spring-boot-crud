58e074337cc8de6d66b657d03ae40a1ad7ed4b12
Micah Smith <86262666+MicahWSmith@users.noreply.github.com>
2022-02-24 17:50:19 -0500

    cors

--- src/main/java/com/example/demo/fund/FundController.java
+++ src/main/java/com/example/demo/fund/FundController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 public class FundController {
 
